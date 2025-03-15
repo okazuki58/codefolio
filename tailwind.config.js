c9b3cb0774f6d1e72562d16bc655c389124a8e2a
Kazuki <okazuki58@gmail.com>
2025-03-15 20:46:02 +0900

    style: proseクラス以下の要素のスタイリングを更新

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,26 +10,6 @@ module.exports = {
         sans: ["var(--font-geist-sans)"],
         mono: ["var(--font-geist-mono)"],
       },
-      typography: {
-        DEFAULT: {
-          css: {
-            maxWidth: "100%",
-            img: {
-              marginTop: "1em",
-              marginBottom: "1em",
-            },
-            figure: {
-              marginTop: "1.5em",
-              marginBottom: "1.5em",
-            },
-            h2: {
-              fontWeight: "700",
-              marginTop: "2em",
-              marginBottom: "1em",
-            },
-          },
-        },
-      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
